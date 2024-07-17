feeabc31a0aa76f90701eca2843295f04621dcd6
Kevsl <ktr2@pm.me>
2024-07-17 11:14:53 +0200

    updated admin page fixed useless items-center

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
     return (
-        <main className="flex items-center">
+        <main className="flex">
             <AdminMenu />
             <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8 bg-white w-screen"></div>
         </main>
